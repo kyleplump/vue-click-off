d357eb7e496b7a99804d1909c0a778d6e2bfeb51
Kyle Plump <kplump1395@gmail.com>
2020-03-10 18:33:00 -0400

    ignored elements

--- src/vue-click-off.js
+++ src/vue-click-off.js
@@ -3,18 +3,31 @@ let ignoredElements = [];
 let callback = null;
 
 function clicked(ev) {
-    console.log(ev)
 
-    // if clicked outside
+    // clicked element
     let el = ev.srcElement;
     let clickedOff = true;
 
+    // traverse the DOM to find if the clicked element is a child of 
+    // the bound element, or if it is one of the exluded elements
     while(el.parentNode) {
-        
+    
         if(el === element) {
             clickedOff = false;
         }
+        
+        if(ignoredElements.length) {
+            
+            ignoredElements.forEach((i) => {
 
+                if(i.includes('.')) { i = i.slice(1) }
+
+                if(el.classList.contains(i)) {
+                    clickedOff = false;
+                }
+            });
+        }
+        
         el = el.parentNode;
     }
 
@@ -23,14 +36,11 @@ function clicked(ev) {
     }
 }
 
-export const vueClickOff = {
+exports.clickOff = {
 
     bind: function(el, binding) {
         element = el;
-        ignoredElements = binding.args;
-        console.log(element, ignoredElements)
-        console.log('binding', binding);
-        console.log(callback);
+        ignoredElements = binding.arg;
         callback = binding.value;
         document.addEventListener('click', clicked);
     },
@@ -40,4 +50,3 @@ export const vueClickOff = {
     }
 }
 
-export default vueClickOff;
